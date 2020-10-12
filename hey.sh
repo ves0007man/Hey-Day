fe6072a9e0355cfacb19cae1963a61d84cfcd1ff
ves0007man <ves0007@auburn.edu>
2020-10-12 17:49:34 -0500

    Created a "hey day" interactive script in honor of AU's Hey Day

---
+++ hey.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#User is asked to type their name on the command line
+
+echo "What's your name?"
+
+#The users name is assigned using the read command
+read name
+
+#The user gets a "hey" message
+echo "Hey, $name!" 
